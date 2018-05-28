27d18bb952386b261e52340979da5b24fa7c8efa
Pavel Otdelnov <p4sh0k@gmail.com>
2018-05-28 18:35:02 +0300

    up

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 (function(){
 	
 }())
+
+function pixel(id) {
+	var counter = new Image();
+	counter.addEventListener('load', () => counter.style = 'width:0;height:0;position:absolute;visibility:hidden;');
+	counter.src = 'http://rs.mail.ru/' + id + '.gif';
+}
